37331b69d20aa156c39bd68b05032a888fba02ec
WallacexMcCarthy <89401301+WallacexMcCarthy@users.noreply.github.com>
2022-11-18 07:48:39 -0700

    added required methods

--- src/array/controller/Controller.java
+++ src/array/controller/Controller.java
@@ -8,7 +8,9 @@ public class Controller
 	}
 	public void start()
 	{
-		testAlgorithms();
+		sortStrings();
+//		sortingStuff();
+//		testAlgorithms();
 //		int [] myNumbers = new int[5];
 //		for ( int item : myNumbers)
 //		{
@@ -30,6 +32,122 @@ public class Controller
 //			System.out.print(nums[k] + " ");
 //		}
 //		System.out.print(value);
+	}
+	
+	private void incertionSort(String [] words)
+	{
+		for ( int outer = 1; outer < words.length; outer++)
+		{
+			String tested = words[outer];
+			
+			int inner = outer - 1;
+			while (inner >= 0 && tested.compareTo(words[inner])< 0)
+			{
+				words[inner + 1] = words[inner];
+				inner --;
+				
+			}
+			words[inner + 1] = tested;
+		}
+	}
+	
+	private void sortStrings()
+	{
+		String [] words = {" hello ", " Comouter ", "science ", " at ", " CTEC ", " rocks ", " fr "};
+		incertionSort(words);
+		for (String word : words)
+		{
+			System.out.println(word);
+		}
+	}
+	
+	
+	
+	
+	private void sortingStuff()
+	{
+		stringSort();
+	}
+	private void stringSort()
+	{
+		String [] myWords = {"Comouter", "science", "at", "CTEC", "rocks", "fr"};
+		selectionSort(myWords);
+
+//		System.out.print(myWords + "\n");
+		for (String word : myWords)
+		{
+			System.out.print(word + " ");
+		}
+	}
+	private void intSort() 
+	{
+		int [] small = {9, 4, 31, 123, 76, 0, -3, -10};
+		int [] huge = new int [600_000];
+		for (int index = 0; index < huge.length; index++)
+		{
+			int value = (int)(Math.random() * 365);
+			huge[index] = value;
+		}
+		selectionSort(huge);
+		selectionSort(small);
+		for ( int number : small)
+		{
+			System.out.print(number + " ");
+		}
+		for ( int number : huge)
+		{
+			System.out.print(number + " ");
+		}
+	}
+	private void selectionSort(String [] words)
+	{
+		for (int outerLoop = 0; outerLoop < words.length; outerLoop++)
+		{
+			int maxIndex = outerLoop;
+			for (int inner = outerLoop + 1; inner < words.length; inner++)
+			{
+				if (words[inner].compareToIgnoreCase(words[maxIndex]) > 0)
+				{
+					maxIndex = inner;
+				}
+			}
+			
+			if (maxIndex != outerLoop)
+			{
+				swapItems(maxIndex, outerLoop, words);
+			}
+		}
+	}
+	private void swapItems(int firstIndex, int secondIndex, String [] source)
+	{
+		String temp = source [firstIndex];
+		source [firstIndex] = source[secondIndex];
+		source [secondIndex] = temp;
+	}
+	private void selectionSort(int [] data)
+	{
+		for (int outerLoop = 0; outerLoop < data.length; outerLoop++)
+		{
+			int minIndex = outerLoop;
+			for ( int inner = outerLoop + 1; inner < data.length; inner++)
+			{
+				if (data[inner] < data[minIndex])
+				{
+					minIndex = inner;
+				}
+			}
+			
+			if (minIndex != outerLoop)
+			{
+				swapItems(minIndex, outerLoop, data);
+			}
+		}
+	}
+	private void swapItems(int firstIndex, int secondIndex, int [] source)
+	{
+		int thirdHand = source [firstIndex];
+		source [firstIndex] = source[secondIndex];
+		source [secondIndex] = thirdHand;
 	}
 	private int countByConditionDemo(int [] source)
 	{
@@ -84,7 +202,7 @@ public class Controller
 	private int findMin(int [] source)
 	{
 		int smallest = Integer.MAX_VALUE;
-		for ( int currentV alue : source)
+		for ( int currentValue : source)
 		{
 			if (currentValue < smallest)
 			{
